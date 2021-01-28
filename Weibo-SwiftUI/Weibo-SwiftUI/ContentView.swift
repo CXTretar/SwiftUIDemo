b241b64345d1504bdc85cddf5eb203c54893a339
CXTretar <misscxuan@163.com>
2021-01-28 19:17:15 +0800

    init

---
+++ Weibo-SwiftUI/Weibo-SwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Weibo-SwiftUI
+//
+//  Created by CXTretar on 2021/1/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+            .foregroundColor(.gray)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
